70be32abb9acbef3f059138d8e3d52635813964e
Diogo Silva <diogo.silva.3439.tdgo14@gmail.com>
2020-07-16 20:41:27 +0100

    rdy to deploy

---
+++ client/build/precache-manifest.59af36d7dee5bb85872bc8b6c88470a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbed5c66665cefef60b44911a3e208ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9e84ebc05fad44d320",
+    "url": "/static/css/main.976cb8e6.chunk.css"
+  },
+  {
+    "revision": "90149d7e824d002c1b2f",
+    "url": "/static/js/2.3149588a.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/static/js/2.3149588a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9e84ebc05fad44d320",
+    "url": "/static/js/main.380bb2b0.chunk.js"
+  },
+  {
+    "revision": "bc40466b58c03a59f2a1",
+    "url": "/static/js/runtime-main.229f885b.js"
+  },
+  {
+    "revision": "a110cc631d34cc1f14d7e0a08a0ea64a",
+    "url": "/static/media/Champions.a110cc63.svg"
+  }
+]);
